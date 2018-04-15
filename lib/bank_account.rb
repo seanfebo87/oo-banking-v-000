7e1df492cebe5b07190d53782cb6ac0fc814bb9f
Sean Febo <seanfebo@techie.com>
2018-04-15 05:17:26 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,5 +12,18 @@ class BankAccount
   end
   
   def display_balance
-    "i have $#{@balance} in my account."
+    "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0 
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
