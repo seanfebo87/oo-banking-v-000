7720b8a22fc90fc30c0eb496e460f178c9496a47
Sean Febo <seanfebo@techie.com>
2018-04-15 06:25:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,5 +26,10 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+  
+  def reverse_transfer
+    receiver.balance -= @amount
+    sender.balance += @amount
+  end
     
 end
